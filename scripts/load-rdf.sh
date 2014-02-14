3420c8e5d4884b378aca92fa3b24bf71c798d780
Dipsy Kapoor <dipsykapoor@gmail.com>
2014-02-14 15:51:37 -0800

    updated A.n3. changed load-rdf.sh to accept filename as commandline arg

--- scripts/load-rdf.sh
+++ scripts/load-rdf.sh
@@ -1,10 +1,16 @@
 ### Script to post the N3 files to the triple store
 ### 
-### Execute this script from the main saam-lod directory: sh scripts/load-rdf.sh
+### Execute this script from the main saam-lod directory: sh scripts/load-rdf.sh <n3 filename>
 ###
+
+#!/bin/bash
 
 # Uncomment to clear the repo 
 #curl -X DELETE http://lod.isi.edu/openrdf-sesame/repositories/saam_openrdf/statements
 
 # Post all the N3 files
-curl -X POST -H "Content-Type:text/rdf+n3" -T n3/WebConstituents.n3 http://lod.isi.edu/openrdf-sesame/repositories/saam_openrdf/statements?baseURI='<http://collection.americanart.si.edu/id/>'
+sesameEndpoint=http://lod.isi.edu/openrdf-sesame/repositories/saam_openrdf/statements
+sesameUploadEndpoint=$sesameEndpoint?baseURI='<http://collection.americanart.si.edu/id/>'
+
+echo curl -X POST -H "Content-Type:text/rdf+n3" -T $1 $sesameUploadEndpoint
+curl -X POST -H "Content-Type:text/rdf+n3" -T $1 $sesameUploadEndpoint
