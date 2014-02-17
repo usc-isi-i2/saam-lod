c2ade2eb3e8106846d0ed9b1f4044505a0b1784f
szeke <szekely1401@gmail.com>
2014-02-16 21:41:30 -0800

    Handle multiple models for same table

--- scripts/generate-all-n3.sh
+++ scripts/generate-all-n3.sh
@@ -2,7 +2,10 @@
 
 ### Script to generate the N3 files for SAAM
 ### 
-### Execute this script from the main saam-lod directory: sh scripts/generate-all-n3.sh
+### Execute this script from the main saam-lod directory: 
+###   sh scripts/generate-all-n3.sh
+###   sh scripts/generate-all-n3.sh >run.log 2>&1
+###
 ### It assumes that Karma has the same parent folder as saam-lod, if not, change the karma variable below
 ### Copy the ontologies in saam-lod\preloaded-ontologies-crm folder to the karma\preloaded-ontologies folder
 ###
@@ -17,13 +20,14 @@ cd $karma
 MODELFILES=/$modelDir/*.ttl
 for modelFile in $MODELFILES
 do
-  echo "Processing $modelFile file on table $tablename ..."
   # take action on each file. $f store current file name
   #cat $modelFile
   filename=$(basename $modelFile)
-  tablename=${filename%-model.ttl}   #remove the -model..ttl from the filename. That is assumed to be the tablename.
-  n3Filename=$n3Dir/$tablename.n3
+  tablename=${filename%-model*.ttl}   #remove the -model*.ttl from the filename. That is assumed to be the tablename.
+  outputname=${filename%.ttl}
+  n3Filename=$n3Dir/$outputname.n3    #would be nice to remove -model, note that we can have multiple models for the same table.
 
+  echo "Processing $modelFile file on table $tablename ..."
   mvn exec:java -Dexec.mainClass="edu.isi.karma.rdf.OfflineRdfGenerator" -Dexec.args=" \
 --sourcetype DB --dbtype SQLServer \
 --hostname lod.isi.edu --username sa --password isi4676 \
