7a76fea12cc1fec1eac4866dcc818e864493b2a8
szeke <szekely1401@gmail.com>
2014-02-27 16:35:45 -0800

    Script to separately load the sameAs links in the triple store.

---
+++ scripts/load-sameAs-n3.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+### Script to post the N3 files to the triple store
+### 
+### Execute this script from the main saam-lod directory: sh scripts/load-all-n3.sh
+###
+
+saam=$(pwd)
+n3Dir=$saam/n3
+sameAsn3Dir=$n3Dir/n3-seeAlso
+
+sesameEndpoint=http://lod.isi.edu/openrdf-sesame/repositories/saam_openrdf/statements
+#sesameEndpoint=http://localhost:8080/openrdf-sesame/repositories/saam_openrdf/statements
+
+sesameUploadEndpoint=$sesameEndpoint?baseURI='<http://collection.americanart.si.edu/id/>'
+
+
+# Uncomment to clear the repo 
+# curl -X DELETE $sesameEndpoint
+
+
+#load all sameAs n3 files
+N3FILES=/$sameAsn3Dir/*.n3
+#load all n3 files in the n3 directory
+for n3File in $N3FILES
+do
+  echo "Processing $n3File file..."
+  curl -X POST -H "Content-Type:text/rdf+n3" -T $n3File $sesameUploadEndpoint
+done
