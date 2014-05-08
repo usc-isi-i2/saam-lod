ac18f7a830b4fc4b94495159b0eb3bfbad548354
jlbbj111 <wanjingbuct@gmail.com>
2014-05-08 10:05:48 -0700

    added the script for virtuoso

--- scripts/load-saam-n3-virtuoso.sh
+++ scripts/load-saam-n3-virtuoso.sh
@@ -4,13 +4,17 @@ saam=$(pwd)
 n3Dir=$saam/n3
 seeAlsoDir=$saam/n3/n3-seeAlso
 
+# delete all triples
+# $1 port number;$2 username; $3 password
+isql $1 $2 $3 exec="sparql clear graph <http://collection.americanart.si.edu>;"
+
 
 # upload the files in the directory of n3
 N3FILES=$n3Dir/*.n3
 for n3File in $N3FILES
 do
   echo "Loading $n3File file..."
-  isql 1114 dba dba exec="DB.DBA.TTLP_MT (file_to_string_output ('$n3File'), 'http://collection.americanart.si.edu/id/', 'http://collection.americanart.si.edu');" 
+  isql $1 $2 $3 exec="DB.DBA.TTLP_MT (file_to_string_output ('$n3File'), 'http://collection.americanart.si.edu/id/', 'http://collection.americanart.si.edu');" 
   
 done
 
@@ -19,10 +23,6 @@ N3FILES=$seeAlsoDir/*.n3
 for n3File in $N3FILES
 do
   echo "Loading $n3File file..."
-  isql 1114 dba dba exec="DB.DBA.TTLP_MT (file_to_string_output ('$n3File'), 'http://collection.americanart.si.edu/id/', 'http://collection.americanart.si.edu');" 
+  isql $1 $2 $3 exec="DB.DBA.TTLP_MT (file_to_string_output ('$n3File'), 'http://collection.americanart.si.edu/id/', 'http://collection.americanart.si.edu');" 
 done
 
-#upload file seeAlsoWikipedia-seeAlsoNytimes.rdf
-#rdfFile=$seeAlsoDir/seeAlsoWikipedia-seeAlsoNytimes.n3
-#echo "Loading $n3File file..."
-#isql 1114 dba dba exec="DB.DBA.TTLP_MT (file_to_string_output ('$rdfFile'), 'http://collection.americanart.si.edu/id/', 'http://collection.americanart.si.edu');" 
